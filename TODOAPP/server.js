71f77e73b8d3c36d9a141540bcf995d8cea493af
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-11-13 12:26:59 +0900

    3

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -1,38 +1,32 @@
+// 이 두줄은 익스프레스 라이브러리를 사용하겠다 라는소리
 const express = require('express')
-app.use(express.urlencoded({ extended: true }));
 const app = express()
-const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.gr6juzz.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
-  if (에러) return console.log(에러)
-  app.listen(8080, function() {
-    console.log('listening on 8080')
-  })
+app.use(express.static(__dirname + '/public')) // 폴더를 server.js에 등록해야 폴더안의 파일들 html에서 사용가능 .css .js .jpg 이런파일을 static파일이라고함
+
+// 몽고디비 라이브러리 셋팅
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb사이트에 있던 님들의 DB 접속 URL'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
 })
 
-// 누군가가 /pet 으로 방문을 하면 pet 관련된 안내문을 띄워주자
-// .get(경로, 함수(요청,응답){});
-// http://localhost:8080/pet 으로 접속하면 '펫용품을...' 보여준다!
-app.get('/pet',function(req,res){
-  res.send('펫용품을 쇼핑할 수 있는 페이지입니다.')
-});
+// 8080서버를 엶
+app.listen(8080, () => {
+  console.log('8080')
+})
 
-app.get('/beauty',function(req,res){
-  res.send('뷰티용품을 쇼핑할 수 있는 페이지입니다.')
-});
+// 라우팅
+app.get('/',(요청,응답) => { // 메인페이지 접속시
+  // 응답.send('메인입니다.') // 단순 글씨만 보낼떄
+  응답.sendFile(__dirname + '/index.html') // html파일을 보낼때
+})
 
-// .sendFile(보낼파일경로) 홈페이지 접속했을떄 파일 보내주세요
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html')
-});
-
-app.get('/write', function(req,res) { 
-  res.sendFile(__dirname +'/write.html')
-});
-
-// 어떤 사람이 /add 경로로 post 요청 -> req로 전달
-app.post('/add',function(req,res){
-  res.send('전송완료')
-  console.log(req.body.title);
-  // DB저장
+app.get('/news',(요청,응답) => { // /news 접속시
+  응답.send('오늘 비옴')
 })
